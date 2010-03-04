0d5662bdd3907c7d3899e2e2c96bf0993c0ec9a5
seb.barbier <seb.barbier@31d52982-fd83-11de-bbca-1111db1fd05b>
2010-03-04 23:50:00 +0000

    Bon le Vrai ajout d'une ListeDesStationsPourUneLigne ! En faite faut pas juste commiter pour ajouter des nouveaux fichier mais aussi faire "ajout" et ça je le savais pas.
    git-svn-id: http://iginkotempo.googlecode.com/svn/trunk@24 31d52982-fd83-11de-bbca-1111db1fd05b

---
+++ GinkoTempo/Classes/ListeDesStationsPourUneLigne.h
@@ -0,0 +1,16 @@
+//
+//  ListeDesStationsPourUneLigne.h
+//  GinkoTempo
+//
+//  Created by Sébastien BARBIER on 11/02/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GinkoDataSourceProtocol.h"
+
+@interface ListeDesStationsPourUneLigne : NSObject <UITableViewDataSource,GinkoDataSource> {
+    
+}
+
+@end
---
+++ GinkoTempo/Classes/ListeDesStationsPourUneLigne.m
@@ -0,0 +1,125 @@
+//
+//  ListeDesStationsPourUneLigne.m
+//  GinkoTempo
+//
+//  Created by Sébastien BARBIER on 11/02/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "ListeDesStationsPourUneLigne.h"
+#import "StationsController.h"
+#import "StationTableViewCell.h"
+
+@implementation ListeDesStationsPourUneLigne
+
+// Nom de la page
+- (NSString *)name {
+	return @"Stations";
+}
+
+// Nom dans la TabBar
+- (NSString *)navigationBarName {
+	return @"Liste des stations";
+}
+
+// Aucune idée de ce à quoi ça sert
+- (BOOL)showDisclosureIcon {
+	return NO;
+}
+
+// Icone dans la TabBar
+- (UIImage *)tabBarImage {
+	return [UIImage imageNamed:@"Carte.png"];
+}
+
+// atomic name is displayed in a plain style tableview
+- (UITableViewStyle)tableViewStyle {
+	return UITableViewStylePlain;
+};
+
+
+// return the atomic element at the index 
+- (Station *)StationForIndexPath:(NSIndexPath *)indexPath {
+    
+    return [[[StationsController sharedStationsController] stationsWithInitialLetter:[[[StationsController sharedStationsController] stationNameIndexArray] objectAtIndex:indexPath.section]] objectAtIndex:indexPath.row];
+	// return [[[PeriodicElements sharedPeriodicElements] elementsWithInitialLetter:[[[PeriodicElements sharedPeriodicElements] elementNameIndexArray] objectAtIndex:indexPath.section]] objectAtIndex:indexPath.row];
+    
+} 
+
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    StationTableViewCell *cell = (StationTableViewCell *)[tableView dequeueReusableCellWithIdentifier:@"StationTableViewCell"];
+    if (cell == nil) {
+        cell = [[[StationTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"StationTableViewCell"] autorelease];
+    }
+    
+    // configure cell contents
+    // all the rows should show the disclosure indicator
+    if ([self showDisclosureIcon])
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    
+    // set the element for this cell as specified by the datasource. The atomicElementForIndexPath: is declared
+    // as part of the ElementsDataSource Protocol and will return the appropriate element for the index row
+    cell.station = [self StationForIndexPath:indexPath];
+    
+    cell.labelView.text = cell.station.name;
+    
+    return cell;
+    
+}
+
+
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // this table has multiple sections. One for each unique character that an element begins with
+    // [A,B,C,D,E,F,G,H,I,K,L,M,N,O,P,R,S,T,U,V,X,Y,Z]
+    // return the count of that array
+    return [[[StationsController sharedStationsController] stationNameIndexArray] count];
+}
+
+
+
+- (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView {
+    // returns the array of section titles. There is one entry for each unique character that an element begins with
+    // [A,B,C,D,E,F,G,H,I,K,L,M,N,O,P,R,S,T,U,V,X,Y,Z]
+    return [[StationsController sharedStationsController] stationNameIndexArray];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView sectionForSectionIndexTitle:(NSString *)title atIndex:(NSInteger)index {
+    return index;
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView  numberOfRowsInSection:(NSInteger)section {
+    // the section represents the initial letter of the element
+    // return that letter
+    NSString *initialLetter = [[[StationsController sharedStationsController] stationNameIndexArray] objectAtIndex:section];
+    
+    // get the array of elements that begin with that letter
+    NSArray *elementsWithInitialLetter = [[StationsController sharedStationsController] stationsWithInitialLetter:initialLetter];
+    
+    // return the count
+    
+    return [elementsWithInitialLetter count];
+}
+
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    // this table has multiple sections. One for each unique character that an element begins with
+    // [A,B,C,D,E,F,G,H,I,K,L,M,N,O,P,R,S,T,U,V,X,Y,Z]
+    // return the letter that represents the requested section
+    // this is actually a delegate method, but we forward the request to the datasource in the view controller
+    
+    return [[[StationsController sharedStationsController] stationNameIndexArray] objectAtIndex:section];
+}
+
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
